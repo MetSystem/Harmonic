6f66c9c2fbf148567072b3049a391d11ac11d5e8
met <289755007@qq.com>
2021-01-08 12:00:50 +0800

    add

--- Power.TykdStream/StreamService/Impl/TykdStreamService.cs
+++ Power.TykdStream/StreamService/Impl/TykdStreamService.cs
@@ -1,9 +1,4 @@
 ﻿using PowerStream.Core;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading;
 using Tianyi;
 
 namespace PowerStreamServer
@@ -12,21 +7,19 @@ namespace PowerStreamServer
     public class TykdStreamService : IProccessService
     {
         private static object lockObj = new object();
-        private PowerOptions PowerOption { get; set; }
         private TianyiService TyService { get; set; }
 
-        public TykdStreamService(PowerOptions option, TianyiService service)
+        public TykdStreamService(TianyiService service)
         {
-            PowerOption = option;
             TyService = service;
         }
 
-        public void Send(StreamInfo info)
+        public void Send(StreamInfo info, PowerOptions powerOptions)
         {
-            info.GlobalParam = PowerOption.Sources.GlobalParam;
-            info.InputParam = PowerOption.Sources.InputParam;
-            info.OutputParam = PowerOption.Sources.OutputParam;
-            info.OutputLink = PowerOption.Sources.ForwardLink;
+            info.GlobalParam = powerOptions.Sources.GlobalParam;
+            info.InputParam = powerOptions.Sources.InputParam;
+            info.OutputParam = powerOptions.Sources.OutputParam;
+            info.OutputLink = powerOptions.Sources.ForwardLink;
             info.SourceLink = TyService.GetExtGetPlayUrlHX(info.StreamName, 0);
         }
     }
--- Power.WebStream/StreamService/Impl/WebStreamService.cs
+++ Power.WebStream/StreamService/Impl/WebStreamService.cs
@@ -6,7 +6,7 @@ namespace Power.WebStream
     [AliasName("Web")]
     public class WebStreamService : IProccessService
     {
-        public void Send(StreamInfo info)
+        public void Send(StreamInfo info, PowerOptions powerOptions)
         {
             WebClient webClient = new WebClient();
             info.SourceLink = webClient.DownloadString("http://183.131.138.61:9080/video?type=Rtmp&sourceid=0000000000200000000000001244435:0000000000140000000000001231833:192.168.10.148:331100");
